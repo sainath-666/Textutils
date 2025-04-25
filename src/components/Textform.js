182e70c5560f6cbe8734f4d96dd916f8c08ca56e
Sainathreddy <sai65265@gmail.com>
2025-04-26 00:26:04 +0530

    Added lowercase and preview and summary features

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const handleOnChange = (event)=> {
-        setText(event.target.value);
-
-    }
-
-
-    const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("Enter text here");
   return (
-    <div>
-      <div className="mb-3">
-        <h1>{props.heading}</h1>
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
+      <div className="container my-3">
+        <h2> Your Text Summary </h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+
+    </>
   );
 }
