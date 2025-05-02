bd9cf5d3f2f402ff38df6554d5aa0fdaee65965f
Sainathreddy <sai65265@gmail.com>
2025-05-03 01:01:46 +0530

    Added Alert component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,14 +4,17 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert(": Converted to Uppercase!", "success");
   };
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert(": Converted to Lowercase!", "success");
   };
   const handleClearClick = () => {
     let newText = "";
     setText(newText);
+    props.showAlert(": Text Cleared!", "success");
   };
 
   const handleOnChange = (event) => {
@@ -20,11 +23,13 @@ export default function TextForm(props) {
 
   const handleCopy = () => {
     navigator.clipboard.writeText(text);
+    props.showAlert(": Text Copied to Clipboard!", "success");
   };
 
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert(": Extra Spaces Removed!", "success");
   };
 
   const [text, setText] = useState("");
