126fa20c28eca05d3f416988f057757c0dff074a
Sainathreddy <sai65265@gmail.com>
2025-05-05 02:53:45 +0530

    Final commit message

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -76,7 +76,7 @@ export default function TextForm(props) {
       >
         <h2> Your Text Summary </h2>
         <p>
-          {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+          {text.split(/\s/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
         <h2>Preview</h2>
